4e28e7192a5cb7ce13219ba84bdb97c1ad3597ca
Ajayksoni98 <ajayksoni198@gmail.com>
2020-08-07 21:17:40 +0530

    first commit

---
+++ Coffee Machine/Problems/Right triangle/task.py
@@ -0,0 +1,12 @@
+class RightTriangle:
+    def __init__(self, hyp, leg_1, leg_2):
+        self.c = hyp
+        self.a = leg_1
+        self.b = leg_2
+        # calculate the area here
+
+
+# triangle from the input
+input_c, input_a, input_b = [int(x) for x in input().split()]
+
+# write your code here
